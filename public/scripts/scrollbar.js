9af330522263ede6d63e2a23d5179a1a75558618
xRaxyyy <159126222+xRaxyyy@users.noreply.github.com>
2025-10-06 18:58:27 +0200

    Update scrollbar.js

--- public/scripts/scrollbar.js
+++ public/scripts/scrollbar.js
@@ -131,3 +131,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.updateScrollbar = updateScrollbar;
 });
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
